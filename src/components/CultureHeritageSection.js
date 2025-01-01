4bbc30c38321ee29987ac64bf68f9be1dd829ff7
Nadia <nadianabila04@gmail.com>
2025-01-01 23:11:22 +0800

    baru buat tajuk

--- src/components/CultureHeritageSection.js
+++ src/components/CultureHeritageSection.js
@@ -11,18 +11,22 @@ function CultureHeritageSection() {
                 <h2>Culture & Heritages</h2>
                 <Link to="/culture-heritage" className="see-more-button">See More <FontAwesomeIcon icon={faArrowCircleRight}/></Link>
             </div>
+            <div className="description">Immerse yourself in the vibrant traditions, historic landmarks, and cultural treasures that make Penang unique. Discover stories of the past and celebrate the rich heritage that lives on today.</div>
             <div className="heritage-grid">
                 <div className="heritage-item">
-                    {/*<img src="/images/heritage1.jpg" alt="Heritage 1" />*/}
-                    <h3>Temples</h3>
+                    <img src="/images/heritage1.jpg" alt="Heritage 1" />
+                    <h3>Kek Lok Si Temple</h3>
                 </div>
                 <div className="heritage-item">
                     {/*<img src="/images/heritage2.jpg" alt="Heritage 2" />*/}
-                    <h3>Museums</h3>
+                    <h3>Pinang Peranakan Mansion</h3>
                 </div>
                 <div className="heritage-item">
                     {/*<img src="/images/heritage3.jpg" alt="Heritage 3" />*/}
-                    <h3>Festivals</h3>
+                    <h3>George Town UNESCO World Heritage Site</h3>
+                </div>
+                <div className="heritage-item">
+                    <h3>Armenian Street</h3>
                 </div>
             </div>
         </div>
