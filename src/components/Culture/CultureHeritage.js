03274789f91bce1b35373118fbf82a2d3c1f09f9
AinaWahida <ainaroshimi@gmail.com>
2025-01-02 00:55:31 +0800

    tambah header page

--- src/components/Culture/CultureHeritage.js
+++ src/components/Culture/CultureHeritage.js
@@ -1,8 +1,25 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 const CultureHeritage = () => {
     return (
-        <div>
+        <div className="page">
+            <div className="intro">
+                <nav className="bar">
+                    <div className="logo">
+                        <h1>visitPenang.my</h1>
+                    </div>
+                    <ul className="Menu">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/trourist-attraction">Places to Go</Link></li>
+                        <li><Link to="/food-beverages">Yummy Things</Link></li>
+                        <li><Link to="/hotels">Places to Go</Link></li>
+                    </ul>
+                </nav>
+                <div className="intro-content">
+                    <h1>Culture & Heritage</h1>
+                </div>
+            </div>
             <h2>Culture & Heritage</h2>
             <p>Learn about Penang's rich culture and history!</p>
         </div>
