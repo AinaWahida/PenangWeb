3995b385d15c552d4498b852bb51d26466b840bd
Nadia <nadianabila04@gmail.com>
2025-01-03 10:57:21 +0800

    page culture

--- src/components/Culture/CultureHeritage.js
+++ src/components/Culture/CultureHeritage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CultureHeritage.css";
 import {Link} from "react-router-dom";
 import Heritage1 from "./Heritage1";
 
--- src/components/Culture/Heritage1.js
+++ src/components/Culture/Heritage1.js
@@ -15,45 +15,61 @@ function Heritage1() {
     };
 
     return (
-        <div className="section">
+        <div className="heritageSection">
             <h1> Kek Lok Si Temple</h1>
             <div className="Part1">
-                <p>
-                    Kek Lok Si Temple, also known as the "Temple of Supreme Bliss," is one of the largest and most renowned Buddhist temples in Southeast Asia, located on Penang Hill. Built over several decades, this temple complex is a magnificent blend of Chinese, Thai, and Burmese architectural styles. The temple is famous for its grand architecture, including the seven-story pagoda, the towering statue of the Goddess of Mercy (Kuan Yin), and beautiful gardens that attract visitors from all over the world. Kek Lok Si is not just a religious site but also a place for cultural exchange and pilgrimage, especially during the Chinese New Year when thousands of lights illuminate the temple during the annual "lighting up" ceremony.
-                </p>
-                <div className="collage">
-                    <div className="right">
-                        <img src={require("./images-heritage/heritage4.2.jpg")} alt="Heritage 1"
-                             onClick={() => openModal(require("./images-heritage/heritage4.2.jpg"))}/>
-                        <img src={require("./images-heritage/heritage4.3.jpg")} alt="Heritage 2"
-                             onClick={() => openModal(require("./images-heritage/heritage4.3.jpg"))}/>
+                <div className="image-container">
+                    <div className="right-image">
+                        <img
+                            src={require("./images-heritage/heritage1.2.jpg")}
+                            alt="Kek Lok Si Temple 1"
+                            onClick={() => openModal(require("./images-heritage/heritage1.2.jpg"))}
+                        />
                     </div>
+                    <div className="left-image">
+                        <img
+                            src={require("./images-heritage/heritage1.3.jpg")}
+                            alt="Kek Lok Si Temple 2"
+                            onClick={() => openModal(require("./images-heritage/heritage1.3.jpg"))}
+                        />
+                    </div>
+                </div>
+                <div className="description-container">
+                    <p>
+                        Kek Lok Si Temple, also known as the "Temple of Supreme Bliss," is one of the largest and most renowned Buddhist temples in Southeast Asia, located on Penang Hill. Built over several decades, this temple complex is a magnificent blend of Chinese, Thai, and Burmese architectural styles. The temple is famous for its grand architecture, including the seven-story pagoda, the towering statue of the Goddess of Mercy (Kuan Yin), and beautiful gardens that attract visitors from all over the world. Kek Lok Si is not just a religious site but also a place for cultural exchange and pilgrimage, especially during the Chinese New Year when thousands of lights illuminate the temple during the annual "lighting up" ceremony.
+                    </p>
                 </div>
             </div>
             <div className="Part2">
-                <iframe
-                    title="Kek Lok Si Temple"
-                    src="https://www.google.com/maps/place/Kek+Lok+Si+Temple/@5.3996413,100.2736126,17z/data=!3m1!4b1!4m6!3m5!1s0x304ac2185dfc8665:0x2c9084ea7f433ec4!8m2!3d5.3996413!4d100.2736126!16zL20vMDVxMnBo?entry=ttu&g_ep=EgoyMDI1MDEwMS4wIKXMDSoASAFQAw%3D%3D"
-                    width="1200"
-                    height="300"
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade">
-                </iframe>
-                <p>
-                    Entry is free for most parts of the temple, but donations are encouraged to help maintain this cultural landmark.
-                    The temple is especially lively during Chinese New Year when it's beautifully lit with thousands of lanterns.
-                </p>
+                <div className="description-container">
+                    <p>
+                        Entry is free for most parts of the temple, but donations are encouraged to help maintain this cultural landmark. The temple is especially lively during Chinese New Year when it's beautifully lit with thousands of lanterns.
+                    </p>
+                </div>
+                <div className="map-container">
+                    <iframe
+                        title="Kek Lok Si Temple"
+                        src="https://www.google.com/maps/place/Kek+Lok+Si+Temple/@5.3996413,100.2736126,17z/data=!3m1!4b1!4m6!3m5!1s0x304ac2185dfc8665:0x2c9084ea7f433ec4!8m2!3d5.3996413!4d100.2736126!16zL20vMDVxMnBo?entry=ttu&g_ep=EgoyMDI1MDEwMS4wIKXMDSoASAFQAw%3D%3D"
+                        width="100%"
+                        height="300"
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                </div>
             </div>
 
             {isOpen && (
                 <div className="modal" onClick={closeModal}>
-                    <span className="close" onClick={closeModal}>&times;</span>
+                    <span className="close" onClick={closeModal}>
+                        &times;
+                    </span>
                     <img className="modal-content" src={currentImage} alt="Popup" />
                 </div>
             )}
-            </div>
+        </div>
     );
 }
 
 export default Heritage1;
+
