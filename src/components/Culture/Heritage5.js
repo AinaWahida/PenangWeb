352c49a766936ec8f0a175e8db6ad9ccfaff5f18
Nadia <nadianabila04@gmail.com>
2025-01-08 22:28:19 +0800

    culture page

--- src/components/Culture/Heritage5.js
+++ src/components/Culture/Heritage5.js
@@ -16,40 +16,42 @@ function Heritage1() {
 
     return (
         <div className="heritageSection">
-            <h1> Kek Lok Si Temple</h1>
+            <h1> Kapitan Keling Mosque</h1>
             <div className="Part1">
                 <div className="image-container">
                     <div className="right-image">
                         <img
-                            src={require("./images-heritage/heritage1.1.jpg")}
+                            src={require("./images-heritage/heritage5.3.jpg")}
                             alt="Kek Lok Si Temple 1"
-                            onClick={() => openModal(require("./images-heritage/heritage1.1.jpg"))}
+                            onClick={() => openModal(require("./images-heritage/heritage5.3.jpg"))}
                         />
                         <img
-                            src={require("./images-heritage/heritage1.2.jpg")}
+                            src={require("./images-heritage/heritage5.2.jpg")}
                             alt="Kek Lok Si Temple 1"
-                            onClick={() => openModal(require("./images-heritage/heritage1.2.jpg"))}
+                            onClick={() => openModal(require("./images-heritage/heritage5.2.jpg"))}
                         />
                     </div>
 
                     <div className="left-image">
                         <img
-                            src={require("./images-heritage/heritage1.3.jpg")}
+                            src={require("./images-heritage/heritage5.1.jpeg")}
                             alt="Kek Lok Si Temple 1"
-                            onClick={() => openModal(require("./images-heritage/heritage1.3.jpg"))}
+                            onClick={() => openModal(require("./images-heritage/heritage5.1.jpeg"))}
                         />
                         <img
-                            src={require("./images-heritage/heritage1.4.jpeg")}
+                            src={require("./images-heritage/heritage5.4.jpg")}
                             alt="Kek Lok Si Temple 1"
-                            onClick={() => openModal(require("./images-heritage/heritage1.4.jpeg"))}
+                            onClick={() => openModal(require("./images-heritage/heritage5.4.jpg"))}
                         />
                     </div>
                 </div>
 
                 <div className="description-container">
                     <p>
-                        Kek Lok Si Temple, also known as the "Temple of Supreme Bliss," is one of the largest and most
-                        renowned Buddhist temples in Southeast Asia, located on Penang Hill. Built over several decades, this temple complex is a magnificent blend of Chinese, Thai, and Burmese architectural styles. The temple is famous for its grand architecture, including the seven-story pagoda, the towering statue of the Goddess of Mercy (Kuan Yin), and beautiful gardens that attract visitors from all over the world. Kek Lok Si is not just a religious site but also a place for cultural exchange and pilgrimage, especially during the Chinese New Year when thousands of lights illuminate the temple during the annual "lighting up" ceremony.
+                        The Kapitan Keling Mosque, the first Arab-Indian mosque and the largest historic mosque in Georgetown, stands as a significant landmark for visitors to explore in Penang.
+                        With its stunning Moghul architecture, including intricate minarets and domes, it is a must-visit destination.
+                        To enter the mosque, visitors should dress appropriately, as improper attire may result in denied entry.
+                        Additionally, plan your visit outside the five daily Muslim prayer times. The Kapitan Keling Mosque is a popular spot for photography, attracting travelers from around the world and being one of the city's most photographed landmarks.
                     </p>
                 </div>
             </div>
@@ -57,14 +59,14 @@ function Heritage1() {
             <div className="Part2">
                 <div className="description-container">
                     <p>
-                        Entry to most parts of the temple is free, making it accessible to everyone who wishes to experience its serene and spiritual atmosphere. However, visitors are kindly encouraged to contribute through donations, which play a vital role in helping to preserve and maintain this significant cultural and religious landmark for future generations. The temple becomes exceptionally vibrant and enchanting during the Chinese New Year celebrations, as thousands of colorful lanterns and intricate lights are used to illuminate the entire complex, creating a spectacular and festive sight that draws both locals and tourists alike.
+                        In addition, the Kapitan Keling Mosque holds immense cultural and historical significance as a testament to the early Indian Muslim community in Penang. Its architecture, a harmonious blend of Moghul, Moorish, and Indian Islamic influences, is characterized by its grand domes, elegant minarets, and intricate calligraphy, making it a striking visual centerpiece in Georgetown. The mosque’s serene atmosphere is complemented by its well-maintained gardens and fountains, providing a peaceful retreat for visitors. For those interested in delving deeper into its history and significance, guided tours are available, offering insights into Islamic practices and the mosque’s role in fostering community harmony.
                     </p>
                 </div>
 
                 <div className="map-container">
                     <iframe
-                        title="Kek Lok Si Temple"
-                        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3972.113715992545!2d100.2736126!3d5.3996413!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304ac2185dfc8665%3A0x2c9084ea7f433ec4!2sKek%20Lok%20Si%20Temple!5e0!3m2!1sen!2smy!4v1735873148559!5m2!1sen!2smy"
+                        title="Kapitan Keling Mosque"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3972.000065611697!2d100.33454567498413!3d5.416957294562266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x304ac391b89a9aab%3A0x45471342dd49ed63!2sKapitan%20Keling%20Mosque!5e0!3m2!1sen!2smy!4v1736226726845!5m2!1sen!2smy"
                         width="600"
                         height="450"
                         style={{border: 0}}
